2b0c9f93fc7d3d532e311cabc2cd78bcdf3a5d6c
ShayanHaider2005 <k24513@nu.edu.pk>
2025-12-04 10:09:38 +0500

    config issue solved

--- backend/src/main/java/com/example/GetJob/auth/config/CorsConfig.java
+++ backend/src/main/java/com/example/GetJob/auth/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.GetJob.config;
+package com.example.GetJob.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
